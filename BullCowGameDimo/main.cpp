7e016531a8e462669e85ec3a03d2df3961cc80cb
Dimitar Bezanovski <30288047+dimitarbez@users.noreply.github.com>
2019-01-14 23:48:45 +0100

    21 - simplifying with functions

--- BullCowGameDimo/main.cpp
+++ BullCowGameDimo/main.cpp
@@ -5,21 +5,44 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
+
+
+void Introduction();
+string GetGuessAndPrintBack();
+
 
 int main()
 {
-	// introduce the game
-	constexpr int WORD_LENGTH = 5;
-    cout << "Welcome to yeet and skeet, a juicy meme game!\n"; 
-	cout << "Can you guess of the "<<WORD_LENGTH<<" letter isogram ya boi is thinking of?";
-	cout << endl;
+	Introduction();
 
-	// get a guess from the user
-	string Guess = "";
-	getline(cin, Guess);
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
 
 	return 0;
+}
+
+
+void Introduction()
+{
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to yeet and skeet, a juicy meme game!\n";
+	cout << "Can you guess of the " << WORD_LENGTH << " letter isogram ya boi is thinking of?";
+	cout << endl;
+	return;
+}
+
+string GetGuessAndPrintBack()
+{	
+	// asks for a guess
+	cout << "Enter a word: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	// print the guess back
+	cout << "Your guess was: " << Guess << endl;
+
+	return Guess;
 }
