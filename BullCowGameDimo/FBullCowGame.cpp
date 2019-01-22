7cf522f2cb6e26476625147fb4cd346069f5b8bf
Dimitar Bezanovski <30288047+dimitarbez@users.noreply.github.com>
2019-01-22 23:29:19 +0100

    35
    Using struct for simple types

--- BullCowGameDimo/FBullCowGame.cpp
+++ BullCowGameDimo/FBullCowGame.cpp
@@ -11,8 +11,28 @@ void FBullCowGame::Reset()
 {
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
+
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
+
 	MyCurrentTry = 1;
 	return;
+}
+
+// recieves a valid guess, increments turn, returns count
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+	// increment the turn number
+	MyCurrentTry++;
+
+	// setup a return variable
+	BullCowCount BullCowCount;
+
+
+	// loop through all letters in the guess
+		//compare letters against the hidden word
+
+	return BullCowCount;
 }
 
 bool FBullCowGame::CheckGuessValidity(FString)
--- BullCowGameDimo/FBullCowGame.h
+++ BullCowGameDimo/FBullCowGame.h
@@ -5,6 +5,13 @@
 using FString = std::string;
 using int32 = int;
 
+// all integers initialized to zero
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame{
 public:
 	FBullCowGame(); // CONSTRUCTOR
@@ -13,10 +20,12 @@ public:
 	bool IsGameWon() const;
 	bool CheckGuessValidity(FString); // TODO make a more rich return value
 	void Reset(); // TODO make a more rich return value
-	// provide a method for counting bulls and cows and increasing turn number 
+	// provide a method for counting bulls and cows and increasing turn number, assuming valid guess
+	BullCowCount SubmitGuess(FString);
 
 private:
 	// see constructor for initialization
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
+	FString MyHiddenWord;
 };
