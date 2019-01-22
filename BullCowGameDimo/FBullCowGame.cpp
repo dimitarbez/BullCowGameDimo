fa7e314d38430044753cb2e30c71e13179449f54
Dimitar Bezanovski <30288047+dimitarbez@users.noreply.github.com>
2019-01-22 20:41:00 +0100

    31
    Introducing the const keyword

--- BullCowGameDimo/FBullCowGame.cpp
+++ BullCowGameDimo/FBullCowGame.cpp
@@ -1,18 +1,13 @@
 #include "pch.h"
 #include "FBullCowGame.h"
 
-int FBullCowGame::GetMaxTries() { return MyMaxTries; }
-int FBullCowGame::GetCurrentTry() { return MyCurrentTry; }
+int FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+int FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+bool FBullCowGame::IsGameWon() const { return false; }
 
 void FBullCowGame::Reset()
 {
 	return;
-}
-
-
-bool FBullCowGame::IsGameWon()
-{
-	return false;
 }
 
 bool FBullCowGame::CheckGuessValidity(std::string)
