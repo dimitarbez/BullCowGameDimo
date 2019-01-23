520c23198cf6e0899d68a8c4a12f147dfe6ad2ca
dimitarbez <dimitarbez@gmail.com>
2019-01-23 23:55:08 +0100

    45
    Using switch statements

--- BullCowGameDimo/main.cpp
+++ BullCowGameDimo/main.cpp
@@ -11,7 +11,7 @@
 using FText = std::string;
 
 void Introduction();
-FText GetGuess();
+FText GetValidGuess();
 void PlayGame();
 bool AskToPlayAgain();
 
@@ -40,17 +40,37 @@ void Introduction()
 	return;
 }
 
-FText GetGuess() // TODO change to get valid guess
+// loop continuoussly until the user gives a valid guess
+FText GetValidGuess() 
 {	
-	int32 CurrentTry = BCGame.GetCurrentTry();
-	std::cout << "Try " << CurrentTry << ". ";
-	// asks for a guess
-	std::cout << "Enter a word: ";
-	FText Guess = "";
-	std::getline(std::cin, Guess);
+	EGuessStatus Status = EGuessStatus::Invalid_Status;
+	do
+	{
+		int32 CurrentTry = BCGame.GetCurrentTry();
+		std::cout << "Try " << CurrentTry << ". ";
+		// asks for a guess
+		std::cout << "Enter a word: ";
+		FText Guess = "";
+		std::getline(std::cin, Guess);
 
+		Status = BCGame.CheckGuessValidity(Guess);
+		switch (Status)
+		{
+		case EGuessStatus::Not_Isogram:
+			std::cout << "Please enter a word with out any repeating letters.\n";
+			break;
+		case EGuessStatus::Wrong_Length:
+			std::cout << " Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n";
+			break;
+		case EGuessStatus::Not_Lowercase:
+			std::cout << "Please enter all lowercase letters.\n";
+			break;
+		default:
+			return Guess;
+		}
+		std::cout << std::endl;
+	} while (Status != EGuessStatus::OK); // keep looping until we get no errors
 
-	return Guess;
 }
 
 void PlayGame()
@@ -61,17 +81,13 @@ void PlayGame()
 
 	for (int32 i = 0; i < MaxTries; i++)
 	{
-		FText Guess = GetGuess(); // TODO make loop checking valid guesses
-
-		EGuessStatus Status = BCGame.CheckGuessValidity(Guess);
+		FText Guess = GetValidGuess(); 
 
 		// submit valid guess to the game and recieve counts
 		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
-		// print the number of bulls and cows
 
 		std::cout << "Bulls = " << BullCowCount.Bulls << std::endl;
-		std::cout << "Cows = " << BullCowCount.Cows << std::endl;
-		std::cout << std::endl;
+		std::cout << "Cows = " << BullCowCount.Cows << "\n\n";
 	}
 	// TODO add a game summary
 }
